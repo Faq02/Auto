aca81d718c835612510ff249d043942c0acc817a
Faq02 <tarabacik@gmail.com>
2026-03-17 19:54:37 +0400

    update

--- scr/StartFuncs.h
+++ scr/StartFuncs.h
@@ -1,3 +1,19 @@
 #pragma once
 #include <string>
-int startfiles(int line_number, const std::string& file_type);
+class PythonRuntime {
+private:
+    bool m_initialized;  // Флаг состояния
+public:
+    // Конструктор по умолчанию
+    PythonRuntime();
+    ~PythonRuntime();
+    // Удаляем копирование и присваивание
+    PythonRuntime(const PythonRuntime&) = delete;
+    PythonRuntime& operator=(const PythonRuntime&) = delete;
+
+    // Методы для работы с Python
+    int execute(const std::string& code);
+    bool execute_safe(const std::string& code);
+};
+int startfiles(int line_number, const std::string& file_type, PythonRuntime* python, std::string codem);
+std::wstring where_are_you_go_lnk(const std::wstring& lnkPath);
