1e1c0225ae32aefe8ac1596c2699a635e4fec47b
Faq02 <tarabacik@gmail.com>
2026-05-16 19:00:33 +0400

    Update ui_interactions.h

--- scr/ui_interactions.h
+++ scr/ui_interactions.h
@@ -7,3 +7,4 @@
 std::vector<std::wstring> make_massive_of_wstr();
 void countdown(double seconds_to_wait, std::wstring wstring_to_show, int duration);
 int additional_option_logic(std::map<short, std::pair<std::wstring, FileType>>::const_iterator it, int option, size_t orig_lines_count, PythonRuntime& python, short app_type);
+std::wstring colorfulPrint(std::wstring prompt, std::wstring text_color, std::wstring bacground_color = PRINT_BACKGROUNDCOLOR::BLACK);
