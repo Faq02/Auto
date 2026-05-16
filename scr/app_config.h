92d72cde7ec23695f769074789a3a0cdee44a331
Faq02 <tarabacik@gmail.com>
2026-05-16 19:02:33 +0400

    Update app_config.h

--- scr/app_config.h
+++ scr/app_config.h
@@ -29,3 +29,24 @@ static const std::map<std::wstring, std::wstring> sorted_dict = {
     {L"pyautogui.press", L"нажатие клавиши: "},
     {L"pyautogui.moveTo", L"передвинуть мышку на координаты" }
 };
+struct PRINT_TEXTCOLOR {
+    inline static const std::wstring BLACK = L"\033[30m";
+    inline static const std::wstring RED = L"\033[31m";
+    inline static const std::wstring GREEN = L"\033[32m";
+    inline static const std::wstring YELLOW = L"\033[33m";
+    inline static const std::wstring BLUE = L"\033[34m";
+    inline static const std::wstring MAGENTA = L"\033[35m";
+    inline static const std::wstring CYAN = L"\033[36m";
+    inline static const std::wstring WHITE = L"\033[37m";
+};
+
+struct PRINT_BACKGROUNDCOLOR {
+    inline static const std::wstring BLACK = L"\033[40m";
+    inline static const std::wstring RED = L"\033[41m";
+    inline static const std::wstring GREEN = L"\033[42m";
+    inline static const std::wstring YELLOW = L"\033[43m";
+    inline static const std::wstring BLUE = L"\033[44m";
+    inline static const std::wstring MAGENTA = L"\033[45m";
+    inline static const std::wstring CYAN = L"\033[46m";
+    inline static const std::wstring WHITE = L"\033[47m";
+};
