8f1b8b87383d2a0e39f835d9b8cf93bdeae8d208
Faq02 <tarabacik@gmail.com>
2026-06-20 13:20:22 +0400

    Delete Manager.h

--- Manager.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "StartFuncs.h"
-#include "app_config.h"
-
-void manager(short mode, const FileType type, const std::wstring prog_view, bool from_start_files = false, short line_num = NULL, PythonRuntime* python = nullptr);
