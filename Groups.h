f4a441bf6c3961363a4cbdef896c7c5382f0252a
Faq02 <tarabacik@gmail.com>
2026-06-20 13:20:06 +0400

    Delete Groups.h

--- Groups.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-std::wstring group_add(std::wstring prog_view, bool from_changer = false);
-int group_del();
-void create_shortcut(int group_number, wchar_t hotkey_letter, const std::wstring& shortcut_path);
-std::wstring GetProgramsMenuPath(); //AI trash
-std::wstring GetOrCreateAppFolder(); //AI trash
