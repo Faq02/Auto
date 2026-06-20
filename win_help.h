932804b76c568ba17832fe13381acf82e65c24f0
Faq02 <tarabacik@gmail.com>
2026-06-20 13:24:46 +0400

    Delete win_help.h

--- win_help.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <tuple>
-#include <string>
-std::tuple<bool, DWORD> IsProcessRunning(const std::wstring& processName);
-int makeTaskAdmin();
-bool ActivateProcessByPID(DWORD pid);
